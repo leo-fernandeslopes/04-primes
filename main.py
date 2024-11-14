a7d0bf80eb887a9ba5f318bce52e13977a008376
leo-fernandeslopes <leo.fernandeslopes@edu.esiee.fr>
2024-11-14 09:25:12 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-from math import sqrt
+""" Dans main.py, on écrit la fonction isprime(nb) et ses tests automatiques. """
 
 #### Fonction secondaire
 
+def isprime(nb):
+    """
+    La fonction prend en paramètre un entier nb 
+    et renvoie un booléen qui affirme si nb est premier. 
+    """
+    premier = True
 
-def isprime(p):
+    for d in range(2, nb) :
+        if nb % d == 0 :
+            premier = False
 
-    # votre code ici
+    if nb in (0, 1) :
+        premier = False
 
-    pass
+    return premier
+
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    La procédure ne prend pas de paramètre et print les tests de isprime(). 
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
